4c25a2c9ea4d0bbf316ba67fc36283717c83cf25
brittle <brittle@brittle>
2020-10-03 17:08:35 +0100

    [no message]

---
+++ checkConvertAndProcess.py
@@ -0,0 +1,5 @@
+# make sure that all videos exist in webm and apple-compatible mp4
+
+# check transcripts, remove ’, right?’ and ‘, allright?’ leaving a log of which transcripts need uploading to streams (along with the stream id)
+
+# finally, do a git commit & push to git hub
